3bfeffb2cc5fa8103a6e5ab682a3454646705b7b
jiwon111 <dreamjiwon@naver.com>
2022-01-20 01:07:55 +0900

    프로젝트 폴더 생성

--- blog-client/src/App.js
+++ blog-client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SignInForm from './Components/SignIn/SignInForm';
+import { Route, Routes } from 'react-router-dom';
+import PostList from './Pages/PostList';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<SignInForm />} />
+        <Route path="/main" element={<PostList />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
