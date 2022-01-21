aa1215e5903051b3acbfbabccf23b448bba24f27
jiwon111 <dreamjiwon@naver.com>
2022-01-21 14:36:07 +0900

    에디터 폴더 구조

---
+++ blog-client/src/Components/Write/Editor.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Title from './Title';
+import Content from './Content';
+
+const Editor = () => {
+  return <div></div>;
+};
+
+export default Editor;
---
+++ blog-client/src/Components/Write/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Input } from '@mui/material';
+
+const Title = () => {
+  return (
+    <>
+      <Input placeholder="Placeholder" inputProps={ariaLabel} />
+    </>
+  );
+};
+export default Title;
