a77215b8042ebe93a9b60768cdaddd8fe9a267aa
jiwon111 <dreamjiwon@naver.com>
2022-01-21 14:35:53 +0900

    헤더 구현

--- blog-client/src/Components/Header/index.js
+++ blog-client/src/Components/Header/index.js
@@ -1,16 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+import palette from '../../Styles/palette';
 
 const Header = () => {
   return (
     <div
       style={{
-        position: 'absolute',
-        width: '100%',
-        height: '10%',
-        backgroundColor: 'black',
+        width: '100vw',
+        height: '10vh',
+        backgroundColor: `${palette.gray[0]}`,
         boxShadow: '0 2 4 rgba(0, 0, 0, 0.8)',
       }}
-    ></div>
+    >
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          height: 'inherit',
+          alignItems: 'center',
+          marginLeft: '3vw',
+          marginRight: '3vw',
+        }}
+      >
+        <Link
+          to="/main"
+          style={{
+            fontWeight: 'bold',
+            fontSize: '1.8em',
+            color: 'black',
+            textDecoration: 'none',
+          }}
+        >
+          myBlog
+        </Link>
+        <Button variant="contained">로그인</Button>
+      </div>
+    </div>
   );
 };
 export default Header;
