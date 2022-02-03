0a19fef7391b01616397dd5fb6c0716fba548d1d
jiwon111 <dreamjiwon@naver.com>
2022-02-03 21:46:37 +0900

    스타일 수정

--- blog-client/src/Pages/PostListPage.js
+++ blog-client/src/Pages/PostListPage.js
@@ -2,12 +2,21 @@ import React from 'react';
 import Header from '../Components/Header';
 import AddBoxTwoToneIcon from '@mui/icons-material/AddBoxTwoTone';
 import { Button } from '@mui/material';
+import palette from '../Styles/palette';
 
 const PostListPage = () => {
+  const clickHandler = () => {
+    window.location.href = '/write';
+  };
   return (
     <>
       <Header />
-      <Button variant="contained" startIcon={<AddBoxTwoToneIcon />}>
+      <Button
+        variant="contained"
+        startIcon={<AddBoxTwoToneIcon />}
+        onClick={clickHandler}
+        style={{ backgroundColor: `${palette.gray[6]}` }}
+      >
         새 글 작성하기
       </Button>
     </>
