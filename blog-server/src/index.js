0e21bdcd8e164c8b64716d843201ab625664be76
jiwon111 <dreamjiwon@naver.com>
2022-02-08 18:33:55 +0900

    재설치

--- blog-server/src/index.js
+++ blog-server/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const app = new express();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+app.get('/', (req, res) => {
+  res.send('홈');
+});
+
+app.get('/about', (req, res) => {
+  res.send('소개');
+});
+
+app.listen(5000, () => {
+  console.log('5000번 포트에서 대기 중');
+});
