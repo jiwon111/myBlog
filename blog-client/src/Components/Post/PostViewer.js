6a858b901142a2e9f03bb5d730b6d658c48e8bd8
jiwon111 <dreamjiwon@naver.com>
2022-02-21 23:13:01 +0900

    포스트 조회 목록 페이지 구현

---
+++ blog-client/src/Components/Post/PostViewer.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+const PostViewer = () => {};
+export default PostViewer;
--- blog-client/src/Pages/PostListPage.js
+++ blog-client/src/Pages/PostListPage.js
@@ -3,6 +3,7 @@ import Header from '../Components/Header';
 import AddBoxTwoToneIcon from '@mui/icons-material/AddBoxTwoTone';
 import { Button } from '@mui/material';
 import palette from '../Styles/palette';
+import PostList from '../Components/Post/PostList';
 
 const PostListPage = () => {
   const clickHandler = () => {
@@ -15,10 +16,11 @@ const PostListPage = () => {
         variant="contained"
         startIcon={<AddBoxTwoToneIcon />}
         onClick={clickHandler}
-        style={{ backgroundColor: `${palette.gray[6]}` }}
+        style={{ backgroundColor: `${palette.cyan[6]}`, marginTop: '2rem' }}
       >
         새 글 작성하기
       </Button>
+      <PostList />
     </>
   );
 };
